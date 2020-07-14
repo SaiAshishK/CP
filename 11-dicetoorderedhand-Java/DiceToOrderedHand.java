a743a072b5609456115223e559062e489dac3e94
SaiAshishK <saiashishk@msitprogram.net>
2020-07-14 18:03:48 +0530

    Completed

--- 11-dicetoorderedhand-Java/DiceToOrderedHand.java
+++ 11-dicetoorderedhand-Java/DiceToOrderedHand.java
@@ -10,10 +10,45 @@
 // assert(dicetoorderedhand(2,2,2) == 222)
 // Hint: You can use max(a,b,c) to find the largest of 3 values, and
 // min(a,b,c) to find the smallest.
+import java.lang.Math;
 
 public class DiceToOrderedHand {
 	public int diceToOrderedHand(int a, int b, int c) {
 		// your code goes here
-		return 0;
-	}	
+		int max = max(a , b , c);
+		int min = min(a , b , c);
+		// System.out.println(max+" "+min);
+		int m = 0;
+		if(min <a && a <max){
+			m =a;
+		}
+		else if(min <b && b <max){
+			m = b;
+		}
+		else{
+			m = c;
+		}
+		int res = (max*100) + (m*10) + min;
+		return res;
+	}
+	public int max(int a, int b, int c) {
+		if(a >= b && a >= c){
+			return a;
+		}
+		else if (b >=a && b >=c){
+			return b;
+		}
+		return c;
+	}
+	public int min(int a, int b, int c) {
+		if(a <= b && a <= c){
+			return a;
+		}
+		else if (b <=a && b <=c){
+			return b;
+		}
+		return c;
+	}
+
+
 }
