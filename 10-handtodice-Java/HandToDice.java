6738879ec1df8b4c402e980f3cd56f9ac1766437
SaiAshishK <saiashishk@msitprogram.net>
2020-07-14 17:38:14 +0530

    Completed

--- 10-handtodice-Java/HandToDice.java
+++ 10-handtodice-Java/HandToDice.java
@@ -4,10 +4,25 @@
 // assert(handToDice(214) == (2,1,4))
 // assert(handToDice(422) == (4,2,2))
 // Hint: You might find and % useful here, and also getKthDigit().
+import java.util.Arrays;
 
 public class HandToDice {
-	public int[] handToDice(int hand) {
+	public static int[] handToDice(int hand) {
 		// your code goes here
-		return new int[0];
+		int[] s = new int[3];
+		int res = hand%10;
+		s[2] = res;
+		hand = hand-res;
+		hand = hand/10;
+		int res1 = hand%10;
+		s[1] = res1;
+		// System.out.println(hand);
+		hand = hand-res1;
+		hand = hand/10;
+		s[0] = hand;
+		return s;
+	}
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(handToDice(123)));
 	}
 }
