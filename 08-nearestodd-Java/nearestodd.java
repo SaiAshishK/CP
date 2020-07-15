2c5c6b80860f9a6b5c8b2cc6310dcea25fd874f9
SaiAshishK <saiashishk@msitprogram.net>
2020-07-15 23:46:02 +0530

    Jul 15  23:46:01

--- 08-nearestodd-Java/nearestodd.java
+++ 08-nearestodd-Java/nearestodd.java
@@ -2,11 +2,12 @@
 // # and returns as an int value the nearest odd number to n. 
 // # In the case of a tie, return the smaller odd value. 
 // # Note that the result must be an int, so nearestOdd(13.0) is the int 13, and not the double 13.0.
+import java.lang.Math;
 
 class nearestodd {
 	public int fun_nearestodd(double n){
 		// your code goes here
-		int n1 = (int)n;
+		int n1 = (int)Math.round(n);
 		if(!(n1%2 == 0)){
 			return n1;
 		}
