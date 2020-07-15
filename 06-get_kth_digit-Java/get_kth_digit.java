b0e001ca229cbc0f7f0e7d898a993825a3089529
SaiAshishK <saiashishk@msitprogram.net>
2020-07-15 23:40:48 +0530

    Jul 15  23:40:48

--- 06-get_kth_digit-Java/get_kth_digit.java
+++ 06-get_kth_digit-Java/get_kth_digit.java
@@ -4,11 +4,19 @@
 // # Triangle where the triangle starts at row 0, and 
 // # each row starts at column 0. If either row or col 
 // # are not legal values, return None, instead of crashing. 
+import java.lang.Math;
 
 class get_kth_digit {
 	public int fun_get_kth_digit(int digit, int k){
 		// your code goes here
-		
-		return 0;
+		int d = Math.abs(digit);
+		String[] s = Integer.toString(d).split("");
+		try{
+			int c = Integer.parseInt(s[s.length -k -1]);
+		return c;
+		}
+		catch(Exception e){
+			return 0;
+		}
 	}
 }
