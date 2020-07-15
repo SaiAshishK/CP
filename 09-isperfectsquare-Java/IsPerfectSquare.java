b9aa9c29c78c41eb716b93b4828647bb7a89afcb
SaiAshishK <saiashishk@msitprogram.net>
2020-07-16 00:28:33 +0530

    Jul 16  00:28:33

--- 09-isperfectsquare-Java/IsPerfectSquare.java
+++ 09-isperfectsquare-Java/IsPerfectSquare.java
@@ -2,10 +2,14 @@
 // Write the function isPerfectSquare(n) that takes a possibly-non-int value, and returns True if
 // it is an int that is a perfect square (that is, if there exists an integer m such that
 // m**2 == n), and False otherwise. Do not crash on non-ints nor on negative ints.
+import java.lang.Math;
 
 public class IsPerfectSquare {
 	public boolean isPerfectSquare(int n) {
-		// Your code goes here
-		return false;
+      // Your code goes here
+      double sq = Math.sqrt(n);
+      int w = (int)sq;
+
+		return sq == w;
 	}
 }
