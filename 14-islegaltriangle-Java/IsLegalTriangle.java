0efbd7fa3bee4679b4657c4a57944099e82fc1bf
SaiAshishK <saiashishk@msitprogram.net>
2020-07-15 11:35:50 +0530

    Compleeted

--- 14-islegaltriangle-Java/IsLegalTriangle.java
+++ 14-islegaltriangle-Java/IsLegalTriangle.java
@@ -4,10 +4,21 @@
 // otherwise. Note from the triangle inequality that the sum of each two sides must be greater
 // than the third side, and further note that all sides of a legal triangle must be positive. Hint:
 // how can you determine the longest side, and how might that help?
+import java.lang.Math;
 
 public class IsLegalTriangle {
-	public boolean isLegalTriangle(double s1, double s2, double s3) {
+	public static boolean isLegalTriangle(double s1, double s2, double s3) {
 		// Your code goes here
-		return false;
+		if(s1<0 || s2 < 0 || s3 < 0){
+			return false;
+		}
+
+		// boolean flag = true;
+		if((s1 >= (s2+s3))||(s2 >= (s1+s3))||(s3 >= (s2+s1)))
+		{
+			return false;
+		}
+		return true;
 	}
+	
 }
