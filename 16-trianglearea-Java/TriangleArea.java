33af84b45ea9684c68dcf36a98195812021159c9
SaiAshishK <saiashishk@msitprogram.net>
2020-07-15 14:23:49 +0530

    Jul 15  14:23:49

--- 16-trianglearea-Java/TriangleArea.java
+++ 16-trianglearea-Java/TriangleArea.java
@@ -5,10 +5,13 @@
 import java.lang.Math;
 
 public class TriangleArea {
-	public int triangleArea(double s1, double s2, double s3) {
+	public static int triangleArea(double s1, double s2, double s3) {
 		// Your code goes here
 		int area = ((int)s1 + (int)s2 +  (int)s3)/2;
 		int res = (int)Math.sqrt(area * (area - (int)s1) * (area - (int)s2) * (area - (int)s3));
 		return res;
+	}
+	public static void main(String[] args) {
+		System.out.println(triangleArea(10.0,10.0,10.0));
 	}
 }
