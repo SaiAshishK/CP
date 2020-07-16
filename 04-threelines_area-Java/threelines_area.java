316845487c8bfc4537d6f1de08f85dcae5d65cb8
SaiAshishK <saiashishk@msitprogram.net>
2020-07-16 13:19:04 +0530

    Jul 16  13:19:04

--- 04-threelines_area-Java/threelines_area.java
+++ 04-threelines_area-Java/threelines_area.java
@@ -4,6 +4,8 @@
 import java.lang.Math.*;
 class threelines_area {
 	public int fun_threelines_area(int a, int b, int c){
+		double p = (a+b+c)/2;
+		int area = (int)Math.sqrt(p * (p-a) * (p-b) * (p-c));
 		return 0;
 	}
 }
