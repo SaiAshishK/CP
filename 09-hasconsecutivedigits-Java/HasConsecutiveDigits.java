e18f7ec7f540a0e4328ae50160299e6515338782
SaiAshishK <saiashishk@msitprogram.net>
2020-07-16 18:17:38 +0530

    Jul 16  18:17:37

--- 09-hasconsecutivedigits-Java/HasConsecutiveDigits.java
+++ 09-hasconsecutivedigits-Java/HasConsecutiveDigits.java
@@ -5,6 +5,12 @@
 public class HasConsecutiveDigits {
 	public boolean hasConsecutiveDigits(int n) {
 		// your code goes here
+		String[] s = Integer.toString(n).split("");
+		for(int i = 0 ; i < s.length-1 ; i++){
+			if(s[i].equals(s[i+1])){
+				return true;
+			}
+		}
 		return false;
 	}
 }
