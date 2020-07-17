4a170c02c0f2df353283d0fd71110fd78e8be67b
SaiAshishK <saiashishk@msitprogram.net>
2020-07-17 15:06:14 +0530

    Jul 17  15:06:14

--- 06-replace-Java/replace.java
+++ 06-replace-Java/replace.java
@@ -9,7 +9,7 @@ class replace {
 		String res = "";
 		for(int i =0 ;i < s1.length() ; i++){
 			if(s1.charAt(i) == s2.charAt(0)){
-				// System.out.println("---------"+s1.substring(i, i+s2.length())+"------"+s2);
+				System.out.println("---------"+(i+s2.length())+"------"+s1.length());
 				if(i + s2.length() <= s1.length()){
 					if(s1.substring(i, i+s2.length()).equals(s2)){
 
