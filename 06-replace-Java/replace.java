71d00298f2064d30cd3f72c3d50d06d8916d70d1
SaiAshishK <saiashishk@msitprogram.net>
2020-07-17 14:54:57 +0530

    Jul 17  14:54:57

--- 06-replace-Java/replace.java
+++ 06-replace-Java/replace.java
@@ -6,6 +6,21 @@
 
 class replace {
 	public String fun_replace(String s1, String s2, String s3){
+		String res = "";
+		for(int i =0 ;i < s1.length() ; i++){
+			if(s1.charAt(i) == s2.charAt(0)){
+				if(s1.substring(i, i+s2.length()).equals(s2)){
+					res = res + s3;
+					i = i + s2.length();
+				}
+				else{
+					res = res + s1.charAt(i);
+				}
+			}
+			else{
+				res = res + s1.charAt(i);
+			}
+		}
 		return s1;
 	}
 	
