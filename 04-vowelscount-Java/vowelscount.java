e6273a2a0625274c36b7c6ef8aca71d01b01e65a
SaiAshishK <saiashishk@msitprogram.net>
2020-07-17 14:34:16 +0530

    Jul 17  14:34:16

--- 04-vowelscount-Java/vowelscount.java
+++ 04-vowelscount-Java/vowelscount.java
@@ -7,7 +7,18 @@
 class vowelscount {
 	public int fun_vowelscount(String s){
 		// your code goes here
-		return 0;
+		String[] vowel = {"a","e","i","o","u","A","E","I","O","U"};
+		String[] sp = s.split("");
+		int count = 0;
+		for(String a : sp){
+			for(String g : vowel){
+				if(a.equals(g)){
+					count++;
+					break;
+				}
+			}
+		}
+		return count;
 	}
 	
 }
