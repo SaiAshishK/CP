f9b0f707ce555b3ca4768e31da50b9d60f37c470
SaiAshishK <saiashishk@msitprogram.net>
2020-07-18 20:31:11 +0530

    Jul 18  20:31:11

--- 11-smallestdifference-Java/SmallestDifference.java
+++ 11-smallestdifference-Java/SmallestDifference.java
@@ -13,7 +13,7 @@ public class SmallestDifference {
         if(a.length == 0){
             return -1;
         }
-        int res = 500;
+        int res = Integer.MAX_VALUE;
         for(int i = 0 ; i < a.length-1 ; i++){
             int c = Math.abs(a[i] - a[i+1]);
             System.out.println(c);
