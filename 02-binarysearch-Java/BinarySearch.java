50ab4688ba81d6406c3ba01902ed5af6a0c95ef0
SaiAshishK <saiashishk@msitprogram.net>
2020-07-19 12:37:08 +0530

    Jul 19  12:37:08

--- 02-binarysearch-Java/BinarySearch.java
+++ 02-binarysearch-Java/BinarySearch.java
@@ -1,6 +1,13 @@
 class BinarySearch {
 	public int binary_search(int[] arr, int value){
         // Your code goes here
+        int count = 0;
+        for(int i = 0 ; i < arr.length ; i++){
+                if(arr[i] == value){
+                        return count;
+                }
+                count++;
+        }
         return -1;
 	}
 }
