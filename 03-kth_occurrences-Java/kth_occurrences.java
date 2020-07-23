5c7327051f8e9b67c6b917774524071f2f213fe4
SaiAshishK <saiashishk@msitprogram.net>
2020-07-24 05:24:32 +0530

    Pending

--- 03-kth_occurrences-Java/kth_occurrences.java
+++ 03-kth_occurrences-Java/kth_occurrences.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 // # Given a string str and an integer K, the task is to find the K-th most 
 // # frequent character in the string. If there are multiple characters that 
 // # can account as K-th the most frequent character then, print any one of them.
@@ -5,6 +8,19 @@
 
 public class kth_occurrences {
 	public char fun_kth_occurrences(String s, int n){
+		ArrayList<String> ar = new ArrayList<String>();
+		HashMap<String,Integer> hm = new HashMap<String,Integer>();
+		String[] sp = s.split("");
+		for(String d : sp){
+			if(ar.contains(d)){
+				hm.replace(d, hm.get(d)+1);
+			}
+			else{
+				ar.add(d);
+				hm.put(d, 1);
+			}
+		}
+		
 		return '0';
 	}
 }
