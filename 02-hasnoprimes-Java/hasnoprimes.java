3b5087fed0926978e6fb398a666dc50213f6cc17
SaiAshishK <saiashishk@msitprogram.net>
2020-07-23 13:37:56 +0530

    Completed

--- 02-hasnoprimes-Java/hasnoprimes.java
+++ 02-hasnoprimes-Java/hasnoprimes.java
@@ -3,7 +3,28 @@
 
 
 class hasnoprimes {
-	public boolean fun_hasnoprimes(int[][] arr){
+
+	public static boolean isPrime(int n) {
+		if(n <=1){
+			return false;
+		}
+		for(int i = 2; i < n;i++){
+			if(n%i == 0){
+				return false;
+			}
+			
+		}
 		return true;
+	}
+	public boolean fun_hasnoprimes(int[][] arr){
+		for(int p = 0 ; p < arr.length ; p++){
+			for(int q = 0 ; q < arr[p].length ; q++){
+				if(isPrime(arr[p][q]) == true){return false;}
+			}
+		}
+		return true;
+	}
+	public static void main(String[] args) {
+		System.out.println(isPrime(4));
 	}
 }
