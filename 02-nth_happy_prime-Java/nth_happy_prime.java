0f086ed41b32fb5789f72a5a7d7ada93e6bde941
SaiAshishK <saiashishk@msitprogram.net>
2020-07-24 08:57:20 +0530

    Completed

--- 02-nth_happy_prime-Java/nth_happy_prime.java
+++ 02-nth_happy_prime-Java/nth_happy_prime.java
@@ -4,8 +4,49 @@
 
 
 class nth_happy_prime {
+	public static boolean isHappyNumber(int n) {
+		// your code goes here
+		int count = 0;
+		while(count < 10 && n != 1){
+			int p  =0;
+			while(n  > 0){
+				int q = n%10;
+				n = n/10;
+				q = (int)Math.pow(q,2);
+				p+=q;
+
+			}
+			n = p;
+			count++;
+		}
+		if(n == 1){
+			return true;
+		}
+
+		return false;
+	}
+
+	public static boolean isPrime(int n) 
+    {
+        if (n <= 1) 
+            return false; 
+   
+        for (int i = 2; i < n; i++) 
+            if (n % i == 0) 
+                return false; 
+  
+        return true; 
+    } 
 	public int fun_nth_happy_prime(int n){
 		// your code goes here
-		return 0;	
+		int count = 0;
+		int p  = 0;
+		while(count-1 != n){
+			p++;
+			if(isHappyNumber(p) && isPrime(p)){
+				count++;
+			}
+		}
+		return p;	
 	}
 }
