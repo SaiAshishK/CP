c89da2bb3fc15d9fa4e0cff87f30c207eb8ad3d2
SaiAshishK <saiashishk@msitprogram.net>
2020-07-27 04:31:55 +0530

    Jul 27  04:31:55

--- 09-nth_palindromic_prime-Java/nth_palindromic_prime.java
+++ 09-nth_palindromic_prime-Java/nth_palindromic_prime.java
@@ -8,4 +8,27 @@ class nth_palindromic_prime {
 	public int fun_nth_palindromic_prime(int n){
 		return 0;
 	}
+	public static boolean isPalindrome(int s1,int s2){
+		String s11 = Integer.toString(s1);
+		String res = "";
+		for(int i = s11.length()-1 ;i >= 0 ; i--){
+			res = res + s11.charAt(i);
+		}
+		if(s2 == Integer.parseInt(res)){
+			return true;
+		}
+		return false;
+	}
+	public static boolean isPrime(int n) 
+    {
+        if (n <= 1) 
+            return false; 
+   
+        for (int i = 2; i < n; i++) 
+            if (n % i == 0) 
+                return false; 
+  
+        return true; 
+    } 
+
 }
