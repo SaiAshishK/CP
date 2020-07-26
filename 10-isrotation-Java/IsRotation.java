804db43c2e2cc387b383446924df6aa361bcf562
SaiAshishK <saiashishk@msitprogram.net>
2020-07-27 04:21:44 +0530

    Jul 27  04:21:44

--- 10-isrotation-Java/IsRotation.java
+++ 10-isrotation-Java/IsRotation.java
@@ -32,7 +32,7 @@ public class IsRotation {
 	}
 	public static String rev(String s){
 		String res = "";
-		for(int i = s.length()-1 ;i < 0 ; i--){
+		for(int i = s.length()-1 ;i <= 0 ; i--){
 			res = res + s.charAt(i);
 		}
 		System.out.println(res);
