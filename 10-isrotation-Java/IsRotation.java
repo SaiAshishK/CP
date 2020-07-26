dcd5f44f23ad5d4822ac339dc89cefe054de4b66
SaiAshishK <saiashishk@msitprogram.net>
2020-07-27 04:12:28 +0530

    Jul 27  04:12:28

--- 10-isrotation-Java/IsRotation.java
+++ 10-isrotation-Java/IsRotation.java
@@ -26,6 +26,6 @@ public class IsRotation {
 		return false;
 	}
 	public static void main(String[] args) {
-		System.out.println(isRotation(1234, 3412));
+		System.out.println(isRotation(3142, 1234));
 	}
 }
