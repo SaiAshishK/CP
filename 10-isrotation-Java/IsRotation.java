4d2b1a45a36cb5035e1324018a01479f5c77d94f
SaiAshishK <saiashishk@msitprogram.net>
2020-07-27 04:16:07 +0530

    Jul 27  04:16:06

--- 10-isrotation-Java/IsRotation.java
+++ 10-isrotation-Java/IsRotation.java
@@ -19,8 +19,9 @@ public class IsRotation {
 				p = i;
 			}
 		}
+		System.out.println("  p  "+p);
 		String s2 = x1.substring(p, x1.length()) + x1.substring(0, p);
-
+		System.out.println("-----"+s2);
 		if(s2.equals(y1) || x1.equals(y1)){
 			return true;
 		}
