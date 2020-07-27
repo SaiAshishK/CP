a9509ae85cf6f776be986a21d09ed829c3401ce2
SaiAshishK <saiashishk@msitprogram.net>
2020-07-27 14:15:49 +0530

    Jul 27  14:15:49

--- 08-nth_circularprime-Java/NthCircularPrime.java
+++ 08-nth_circularprime-Java/NthCircularPrime.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 // nthCircularPrime(n) [20 pts]
 // Write the function nthCircularPrime that takes a non-negative int n and returns the nth 
 // Circular prime, which is a prime number that does not contain any 0's and such that all the 
@@ -8,6 +10,41 @@
 public class NthCircularPrime {
 	public long nthCircularPrime(int n) {
 		// your code goes here
-		return -1;
+		int count = -1;
+		int p  =0;
+		boolean flag = true;
+		while(count != n){
+			p++;
+			if(isPrime(p)){
+				for(int as : combi(p)){
+					if(isPrime(as)){
+						count++;
+					}
+				}
+			}
+		}
+		return p;
 	}
+	public static ArrayList<Integer> combi(int n){
+		ArrayList<Integer> ar = new ArrayList<Integer>();
+		String s = Integer.toString(n);
+		for(int i = 0 ; i < n ; i++){
+			String s1 = s.substring(i,s.length()) + s.substring(0,i);
+			ar.add(Integer.parseInt(s1));
+		}
+		return ar;
+	}
+	public static boolean isPrime(int n) {
+		if(n <=1){
+			return false;
+		}
+		for(int i = 2; i < n;i++){
+			if(n%i == 0){
+				return false;
+			}
+			
+		}
+		return true;
+	}
+
 }
