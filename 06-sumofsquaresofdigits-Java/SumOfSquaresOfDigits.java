f9e1c703e52d921c2accc57dffb5a9701d76e5a6
SaiAshishK <saiashishk@msitprogram.net>
2020-07-27 13:27:30 +0530

    Jul 27  13:27:30

--- 06-sumofsquaresofdigits-Java/SumOfSquaresOfDigits.java
+++ 06-sumofsquaresofdigits-Java/SumOfSquaresOfDigits.java
@@ -3,10 +3,15 @@
 // assert(sumOfSquaresOfDigits(5) == 25)   // 5**2 = 25
 // assert(sumOfSquaresOfDigits(12) == 5)   // 1**2 + 2**2 = 1+4 = 5
 // assert(sumOfSquaresOfDigits(234) == 29) // 2**2 + 3**2 + 4**2 = 4 + 9 + 16 = 29
-
+import java.lang.Math;
 public class SumOfSquaresOfDigits {
 	public long sumOfSquaresOfDigits(int n) {
 		// Your code goes here
-		return -1;
+		String[] s = Integer.toString(n).split("");
+		int res = 0;
+		for(String s1 : s){
+			res = res + (int)Math.pow(Integer.parseInt(s1),2);
+		}
+		return res;
 	}
 }
