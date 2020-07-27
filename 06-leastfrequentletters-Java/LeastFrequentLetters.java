b6899ce4fa00290968cfd22809ab826033adafd1
SaiAshishK <saiashishk@msitprogram.net>
2020-07-27 23:39:12 +0530

    Completed

--- 06-leastfrequentletters-Java/LeastFrequentLetters.java
+++ 06-leastfrequentletters-Java/LeastFrequentLetters.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.*;
+
 // leastFrequentLetters(s) [20 pts]
 // Write the function leastFrequentLetters(s), that takes a string s, and ignoring case (so "A" and "a" are treated 
 // the same), returns a lowercase string containing the least-frequent alphabetic letters that occur in s, each 
@@ -8,8 +12,62 @@
 // if s does not contain any alphabetic characters, the result should be the empty string ("")
 
 public class LeastFrequentLetters {
-	public String leastFrequentLetters(String s) {
+	public static String leastFrequentLetters(String s) {
 		// Your code goes here
-		return "";
+		ArrayList<String> ar = new ArrayList<String>(Arrays.asList("!","?","'",""," "));
+		String res = "";
+		String[] sp = s.split("");
+		for(String s1 : sp){
+			if(!ar.contains(s1)){
+				res = res + s1;
+			}
+		}
+		if(res.equals("")){
+			return "";
+		}
+		res = res.toLowerCase();
+		ArrayList<String> ar1 = new ArrayList<String>();
+		HashMap<String,Integer> hm = new HashMap<String,Integer>();
+		String[] res1 = res.split("");
+		for(String q : res1){
+			if(ar1.contains(q)){
+				hm.replace(q, hm.get(q)+1);
+			}
+			else{
+				ar1.add(q);
+				hm.put(q,1);
+			}
+		}
+		hm = sortByValue(hm);
+		int p1  = 1;
+		String[] p = hm.values().toString().split("");
+		int count = Integer.parseInt(p[1]);
+		if(count == Integer.parseInt(p[4])){
+			p1++;
+		}
+			String g = hm.keySet().toString();
+		if(p1 == 1){String re = Character.toString(g.charAt(1)); return re;}
+		String re1 = "" + g.charAt(1) + g.charAt(4);
+		return re1;
+		// return o;
 	}
+	public static HashMap<String, Integer> sortByValue(HashMap<String, Integer> hm) 
+    { 
+        List<Map.Entry<String, Integer> > list = 
+               new LinkedList<Map.Entry<String, Integer> >(hm.entrySet()); 
+  
+        Collections.sort(list, new Comparator<Map.Entry<String, Integer> >() { 
+            public int compare(Map.Entry<String, Integer> o1,  
+                               Map.Entry<String, Integer> o2) 
+            { 
+                return (o1.getValue()).compareTo(o2.getValue()); 
+            } 
+        }); 
+          
+        HashMap<String, Integer> temp = new LinkedHashMap<String, Integer>(); 
+        for (Map.Entry<String, Integer> aa : list) { 
+            temp.put(aa.getKey(), aa.getValue()); 
+        } 
+        return temp; 
+    }
 }
