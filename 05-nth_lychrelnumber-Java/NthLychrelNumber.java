526b4c6c321f0d6fd7a8e617b4f1a8f6d51b8110
SaiAshishK <saiashishk@msitprogram.net>
2020-07-27 23:00:36 +0530

    Completed

--- 05-nth_lychrelnumber-Java/NthLychrelNumber.java
+++ 05-nth_lychrelnumber-Java/NthLychrelNumber.java
@@ -5,8 +5,67 @@
 // The first few Lychrel numbers are 196, 295, 394, 493, 592, 689, 691, 788, 790, 879, 887….
 
 public class NthLychrelNumber {
-	public long nthLychrelNumber(int n) {
+	public static long nthLychrelNumber(int n) {
 		// Your code goes here
-		return -1;
+		int count = 0;
+		long p  = 150;
+		while(count != n){
+			p++;
+			if(isl(p)){
+				count++;
+			}
+		}
+		return p;
+	}
+	public static boolean ispal(long n){
+		
+		if(n < 0){
+			
+			n = -n;
+		}
+		long c =  0;
+		String[] s = Long.toString(n).split("");
+		if(s.length == 1){
+			return true;
+		}
+		for(int i = s.length-1 ; i >= 0 ;i--){
+			c = c*10 + Integer.parseInt(s[i]);
+		}
+		
+		if(c == n){
+			return true;
+		}
+		return false;
+	}
+	public static long pal(long n){
+		boolean flag = true;
+		if(n < 0){
+			flag = false;
+			n = -n;
+		}
+		if(n < 10){
+			return n;
+		}
+		long c = 0;
+		String[] s = Long.toString(n).split("");
+		for(int i = s.length-1 ; i >= 0 ;i--){
+			c = c*10 + Integer.parseInt(s[i]);
+		}
+		if(flag == false){c = -c;}
+		return c;
+	} 
+	public static boolean isl(long n){
+		int count = 0;
+		while(count != 25 && !ispal(n)){
+			n = n + pal(n);
+			count++;
+		}
+		if(!ispal(n)){
+			return true;
+		}
+		return false;
+	}
+	public static void main(String[] args) {
+		System.out.println(nthLychrelNumber(20));
 	}
 }
