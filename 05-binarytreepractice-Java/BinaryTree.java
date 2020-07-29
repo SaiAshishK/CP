98055b459963f84a4a8341aefc16e3af03ea4726
SaiAshishK <saiashishk@msitprogram.net>
2020-07-29 16:47:12 +0530

    Completed

--- 05-binarytreepractice-Java/BinaryTree.java
+++ 05-binarytreepractice-Java/BinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 class Node {
 	public int value;
@@ -13,12 +15,30 @@ public class BinaryTree {
 	public Node root;
 	
 	public BinaryTree(int value) {
-        // Your code goes here
+		// Your code goes here
+		this.root = new Node(value);
 	}
 
 	public boolean search(int value) {
 		// Your code goes here
-		return true;
+		boolean flag = false;
+		Node temp = root;
+		Queue <Node> qu = new LinkedList<Node>();
+		qu.add(temp);
+		while(qu.isEmpty() == false){
+			Node rem = qu.remove();
+			if(rem.value == value){
+				flag = true;
+				break;
+			}
+			if(rem.right != null){
+				qu.add(rem.right);
+			}
+			if(rem.left != null){
+				qu.add(rem.left);
+			}
+		}
+		return flag;
 	}
 
 	private boolean search_Node(Node node, int value) {
