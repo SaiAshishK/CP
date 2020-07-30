c35aef9d4232f1f276822095ee5b0afe481246d3
SaiAshishK <saiashishk@msitprogram.net>
2020-07-31 03:53:10 +0530

    Pending

--- 06-bstpractice-Java/BST.java
+++ 06-bstpractice-Java/BST.java
@@ -17,21 +17,68 @@ public class BST {
     }
 
     public void insert(int value) {
-    	// Your code goes here
+        // Your code goes here
+        root = insert(root,value);
     }
-
-    private void insert(Node node, int value) {
-    	// Your code goes here
+    private Node insert(Node node, int value) {
+        // Your code goes here
+        if(node == null){
+            System.out.println("2"+node);
+            node = new Node(value);
+            System.out.println(root.left.value);
+            return null;
+        }
+        if(value < node.value){
+            System.out.println("1"+node.left);
+            node.left = insert(node.left,value);
+        }
+        else{    // System.out.println("1");
+            node.right = insert(node.right,value);
+        }
+        return null;
     }
+    
 
     public boolean search(int value) {
-    	// Your code goes here
-    	return false;
+        // Your code goes here
+        if(root.value == value){
+            return true;
+        }
+        else{
+            // System.out.println("1");
+            return search(root,value);
+        }
     }
 
     private boolean search(Node current, int value) {
-    	// Your code goes here
-    	return false;
+        // Your code goes here
+        if(current == null){
+            // System.out.println("4");
+            return false;
+        }
+        if(current.value == value){
+            // System.out.println("3");
+            return true;
+        }
+        if(current.value > value){
+            return search(current.left,value);
+        }
+        // System.out.println("2");
+    	return search(current.right, value);
+    }
+    public void as(){
+        System.out.println(root.value);
+        System.out.println(root.left.value);
+        // System.out.println(root.left.left.value);
+        // System.out.println(root.left.right.value);
+        // System.out.println(root.right.value);
+
+
+    }
+    public static void main(String[] args) {
+        BST b = new BST(4);
+        b.insert(2);
+        b.as();
     }
 
 }
