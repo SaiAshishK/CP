ef4ad44aa7c44dabcee493b42b9dff576384ba5c
SaiAshishK <saiashishk@msitprogram.net>
2020-08-01 02:33:02 +0530

    cOMPLETED

--- 04-recursion_powersof3ton-Java/PowersOf3ToN.java
+++ 04-recursion_powersof3ton-Java/PowersOf3ToN.java
@@ -8,24 +8,43 @@ public class PowersOf3ToN {
 	static int p = -1;
 	public int[] powersOf3ToN(double n) {
 		// Your code goes here
+
 		if(n < 1){
 			return new int[0];
 		}
 		if(p == -1){
-			arr = new int[(int)n/3];
+			arr = new int[pp((int)n)];
 			p++;
 		}
 		arr[p] = (int)Math.pow(3,p);
 		p++;
-		if(p == (((int)n/3))){
+		if(p == (pp((int)n))){
 			p = -1;
 			return arr;
 		}
 		return powersOf3ToN(n);
 	}
+	public int pp(int n){
+		while(!ispow3(n)){
+			n--;
+		}
+		int res = 1;
+		while(n != 1){
+			n = n/3;
+			res++;
+		}
+		return res;
+
+	}
+	public boolean ispow3(int n){
+		while (n % 3 == 0) {
+			n /= 3;
+		}
+		return n == 1;
+	}
 	public static void main(String[] args) {
 		PowersOf3ToN p = new PowersOf3ToN();
-		System.out.println(Arrays.toString(p.powersOf3ToN(1)));
-		System.out.println(Arrays.toString(p.powersOf3ToN(3)));
+		System.out.println(p.pp(100));
+		// System.out.println(Arrays.toString(p.powersOf3ToN(3)));
 	}
 }
